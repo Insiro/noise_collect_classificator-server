e16ff7f6f109f44e858bafc88bca36eeeac154e2
Piesignal <insiro@naver.com>
2020-05-08 17:46:55 +0900

    .

---
+++ ModelRunner.py
@@ -0,0 +1,108 @@
+
+from torch import from_numpy
+import librosa
+import numpy as np
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import librosa.display
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels=1, out_channels=32, kernel_size=(3, 3), stride=1).cuda()
+        self.conv2 = nn.Conv2d(
+            in_channels=32, out_channels=64, kernel_size=(3, 3), stride=1).cuda()
+
+        self.conv3 = nn.Conv2d(
+            in_channels=64, out_channels=128, kernel_size=(3, 3), stride=1).cuda()
+        self.conv4 = nn.Conv2d(
+            in_channels=128, out_channels=128, kernel_size=(3, 3), stride=1).cuda()
+
+        self.conv5 = nn.Conv2d(
+            in_channels=128, out_channels=256, kernel_size=(3, 3), stride=1).cuda()
+
+        self.pool = nn.MaxPool2d(2, stride=2)
+
+        self.FC1 = nn.Linear(256, 128).cuda()
+        self.FC2 = nn.Linear(128, 128).cuda()
+        self.FC3 = nn.Linear(128, 9).cuda()
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+
+        x = x.float()
+        x = self.relu(self.conv1(x))
+        x = self.pool(self.relu(self.conv2(x)))
+
+        x = self.relu(self.conv3(x))
+        x = self.pool(self.relu(self.conv4(x)))
+
+        x = self.relu(self.conv5(x))
+
+        x = torch.nn.AvgPool2d(kernel_size=(
+            27, 49), stride=0, padding=0, ceil_mode=False, count_include_pad=True)(x)
+
+        x = x.squeeze()
+
+        x = self.relu(self.FC1(x))
+        x = self.relu(self.FC2(x))
+        x = self.FC3(x)
+
+        return x
+
+
+def input_from_android(audio):
+    model = CNN()
+    model.load_state_dict(torch.load("model-combined-1.pth.tar"))
+    model.cpu()
+    print(audio.shape)
+    audio = np.split(audio, [110250])
+    print("2")
+    audio = audio[0]
+    librosa.output.write_wav('audio.wav', audio, 22050)
+    print("3")
+    audio = np.nan_to_num(audio)
+    print("4")
+    print(audio)
+    # print(audio.shape)
+    S = librosa.feature.melspectrogram(y=audio, n_mels=128, fmax=8000)
+    melspec = librosa.power_to_db(S, ref=np.max)
+
+    # if the input isnt of the specified order then you will get error in the reshape below
+    melspec = melspec.reshape((1, 1, 128, 216))
+
+    outputs = model(from_numpy(melspec))
+
+    softmax = nn.Softmax()
+    outputs = softmax(outputs)
+    print(outputs)
+    return outputs.detach()
+
+
+# def getInputFromAndroid(audio_bitesArray):
+#     # print(audio_bitesArray)
+#     print(np.array(audio_bitesArray))
+#     # print(np.frombuffer(audio_bitesArray, dtype=np.float64))
+#     return input_from_android(np.array(audio_bitesArray))
+
+
+if __name__ == "__main__":
+    # y, sr = librosa.load(path="bicycle 91-8-new.wav", duration=5)
+
+    # the input should be np.ndarray [shape=(n,)], n=110250 for 5 sec. eg(110250,)
+    print(input_from_android(y))
+
+    # the index of the array is the class and the data is the confidency in percentage
+    # 0=Restaurant
+    # 1=market
+    # 2=Bus
+    # 3=Road
+    # 4=Factory
+    # 5=Coffee_shop
+    # 6=Subway
+    # 7=Office
+    # 8=bicycle
---
+++ sockets.py
@@ -0,0 +1,65 @@
+import socket
+import ModelRunner as mr
+from array import array
+import threading
+import numpy as np
+model_delaytime: str = "5s\n"
+buffer_size: int = 1024
+time_size: int = 110250
+host_name: str = '192.9.45.72'
+port_num: int = 3389
+
+# 24648
+
+
+def runServer():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
+        serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        serverSocket.bind((host_name, port_num))
+        serverSocket.listen()
+        while True:
+            clientSocket, addr_info = serverSocket.accept()
+            print("connected")
+            t = threading.Thread(socketSub(clientSocket))
+            t.start()
+    return 0
+
+
+def socketSub(clientSocket):
+    try:
+        clientSocket.send(model_delaytime.encode('utf-8'))
+        # data = bytearray()
+        data = None
+        while True:
+            i = 0
+            # data.extend(clientSocket.recv(buffer_size))
+            arr = []
+            for a in range(0, 6):
+                for i in range(0, 216):
+                    data = clientSocket.recv(buffer_size)
+                    try:
+                        arr = np.append(arr, np.frombuffer(
+                            data, dtype=np.float64))
+                    except Exception as e:
+                        pass
+                print(a)
+            result = mr.input_from_android(arr)
+            print(result)
+            #     if len(data) > time_size:
+            #         print(type(data))
+            #         # print(array(data))
+            #         # print("-----")
+            #         result = mr.getInputFromAndroid(data[:time_size])
+            #         data = data[time_size:]
+            #         print(result)
+            # clientSocket.sendall(result.encode('utf-8'))
+    except Exception as e:
+        print(e)
+        print('terminate')
+
+
+if __name__ == "__main__":
+    print("start NC server")
+    runServer()
+
+    pass
